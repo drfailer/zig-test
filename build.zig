ab7d75e37be85b09c4d3914d650191d12d28aca3
drfailer <drfailer.42@gmail.com>
2025-01-12 21:20:50 -0500

    test config

--- build.zig
+++ build.zig
@@ -1,21 +1,29 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const exe = b.addExecutable(.{
         .name = "zig-test",
         .root_source_file = b.path("src/main.zig"),
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
+        .target = target,
+        .optimize = optimize,
     });
     b.installArtifact(exe);
 
-    const run_cmd = b.addRunArtifact(exe);
+    const exe_cmd = b.addRunArtifact(exe);
 
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    exe_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| exe_cmd.addArgs(args);
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    const tst = b.addTest(.{
+        .root_source_file = b.path("src/list.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const tst_cmd = b.addRunArtifact(tst);
+
+    b.step("run", "Run the app").dependOn(&exe_cmd.step);
+    b.step("test", "Run unit tests").dependOn(&tst_cmd.step);
 }
